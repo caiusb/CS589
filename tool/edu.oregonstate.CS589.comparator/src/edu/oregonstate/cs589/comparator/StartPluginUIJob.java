b5fa39aa30be39bc0995802b6c5e036b84faea90
cdmihai <codobanmihai@gmail.com>
2014-01-29 15:59:13 -0800

    Launch stub for commit comparator

--- tool/edu.oregonstate.CS589.comparator/src/edu/oregonstate/cs589/comparator/StartPluginUIJob.java
+++ tool/edu.oregonstate.CS589.comparator/src/edu/oregonstate/cs589/comparator/StartPluginUIJob.java
@@ -1,23 +1,27 @@
 package edu.oregonstate.cs589.comparator;
 
+import org.eclipse.compare.CompareUI;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Display;
+import org.eclipse.egit.ui.internal.merge.GitCompareEditorInput;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.ui.progress.UIJob;
 
 public class StartPluginUIJob extends UIJob {
 
-	private Activator activator;
-
 	public StartPluginUIJob(String string) {
 		super(string);
-		this.activator = activator;
 	}
 
 	@Override
 	public IStatus runInUIThread(IProgressMonitor monitor) {
-		MessageDialog.openInformation(Display.getDefault().getActiveShell(), "title", "hello world");
+		
+		String compareVersion = null;
+		String baseVersion = null;
+		Repository repository = null;
+		
+		CompareUI.openCompareDialog(new GitCompareEditorInput(compareVersion, baseVersion, repository));
+		
 		return null;
 	}
 }
