e170f1b27152b457664458fbf99a6020d6d27016
cdmihai <codobanmihai@gmail.com>
2014-01-29 17:22:21 -0800

    Instantiate the repository

--- tool/edu.oregonstate.CS589.comparator/src/edu/oregonstate/cs589/comparator/StartPluginUIJob.java
+++ tool/edu.oregonstate.CS589.comparator/src/edu/oregonstate/cs589/comparator/StartPluginUIJob.java
@@ -1,9 +1,13 @@
 package edu.oregonstate.cs589.comparator;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.eclipse.compare.CompareUI;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.egit.ui.internal.merge.GitCompareEditorInput;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.ui.progress.UIJob;
 
@@ -13,12 +17,22 @@ public class StartPluginUIJob extends UIJob {
 		super(string);
 	}
 
+	@SuppressWarnings({ "unused", "restriction" })
 	@Override
 	public IStatus runInUIThread(IProgressMonitor monitor) {
+		Repository repository = null;
+		
+
+		try {
+			File repoFile = Activator.getDefault().getProjectFile("testData/P");
+			repository = new FileRepository(repoFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		
 		String compareVersion = null;
 		String baseVersion = null;
-		Repository repository = null;
 		
 		CompareUI.openCompareDialog(new GitCompareEditorInput(compareVersion, baseVersion, repository));
 		
