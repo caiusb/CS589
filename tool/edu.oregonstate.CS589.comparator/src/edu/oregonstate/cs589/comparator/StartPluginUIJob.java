abc76a26648d565e2a1125d2d2f56372dbffe97a
cdmihai <codobanmihai@gmail.com>
2014-01-29 18:03:05 -0800

    Proof of concept run

--- tool/edu.oregonstate.CS589.comparator/src/edu/oregonstate/cs589/comparator/StartPluginUIJob.java
+++ tool/edu.oregonstate.CS589.comparator/src/edu/oregonstate/cs589/comparator/StartPluginUIJob.java
@@ -8,7 +8,10 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.egit.ui.internal.merge.GitCompareEditorInput;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.ui.progress.UIJob;
 
 public class StartPluginUIJob extends UIJob {
@@ -20,21 +23,24 @@ public class StartPluginUIJob extends UIJob {
 	@SuppressWarnings({ "unused", "restriction" })
 	@Override
 	public IStatus runInUIThread(IProgressMonitor monitor) {
-		Repository repository = null;
-		
 
 		try {
-			File repoFile = Activator.getDefault().getProjectFile("testData/P");
-			repository = new FileRepository(repoFile);
+			File repoFile = Activator.getDefault().getProjectFile("testData/P/.git");
+			Repository repository = new FileRepository(repoFile);
+
+			RevWalk rw = new RevWalk(repository);	
+			System.out.println(repository.getAllRefs());
+			
+			ObjectId cmitId = repository.resolve("040d292f2ea983a918bd5be9d0242c5dcfff9f38");
+			
+			RevCommit compareCommit = rw.parseCommit(repository.resolve("040d292f2ea983a918bd5be9d0242c5dcfff9f38"));
+			RevCommit baseCommit = rw.parseCommit(repository.resolve("2a5d7c13205ffb27cb9dca9875ef1261acba808b"));
+			
+			CompareUI.openCompareDialog(new GitCompareEditorInput(compareCommit.getName(), baseCommit.getName(), repository));
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		
-		String compareVersion = null;
-		String baseVersion = null;
-		
-		CompareUI.openCompareDialog(new GitCompareEditorInput(compareVersion, baseVersion, repository));
 		
 		return null;
 	}
