32cd4796e061157aaf23dea5dc1e7c4bad9b1e72
cdmihai <codobanmihai@gmail.com>
2014-02-07 22:30:27 -0500

    the event persister does its thing

--- tool/edu.oregonstate.CS589.comparator/src/edu/oregonstate/cs589/comparator/EventPersister.java
+++ tool/edu.oregonstate.CS589.comparator/src/edu/oregonstate/cs589/comparator/EventPersister.java
@@ -1,11 +1,35 @@
 package edu.oregonstate.cs589.comparator;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class EventPersister {
 
-	public static void persist(JSONObject obj) {
-		System.out.println(obj.toJSONString());
+	private String fileName;
+	private JSONArray events;
+
+	public EventPersister(String fileName) {
+		this.fileName = fileName;
+		events = new JSONArray();
+	}
+
+	public void persist(JSONObject obj) {
+		events.add(obj);
+	}
+
+	public void persistToFile() {
+		try {
+			Path filePath = Activator.getDefault().getLocalStoragePath().resolve(fileName);
+			System.err.println(filePath);
+			Files.write(filePath, events.toJSONString().getBytes(), StandardOpenOption.CREATE);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
