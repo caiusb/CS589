c56572ff79c978621ec5e1c8278cb668bbda17a5
cdmihai <codobanmihai@gmail.com>
2014-02-10 14:34:53 -0500

    extracted the repo path to a common root dir

--- tool/edu.oregonstate.CS589.comparator/src/edu/oregonstate/cs589/comparator/DataProvider.java
+++ tool/edu.oregonstate.CS589.comparator/src/edu/oregonstate/cs589/comparator/DataProvider.java
@@ -1,5 +1,6 @@
 package edu.oregonstate.cs589.comparator;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,15 +8,16 @@ import java.util.List;
 
 public class DataProvider {
 
+	private static final String ROOT_REPOS_PATH = "rootReposPath";
+
 	private static final String USER_ID = "userID";
 
 	private Properties properties;
 
 	private String userID;
-
-	private List<Task> tasks;
-
 	private Task demoTask;
+	private String rootReposPath;
+	private List<Task> tasks;
 
 	private static class Instance {
 		public static final DataProvider _instance = new DataProvider();
@@ -24,6 +26,7 @@ public class DataProvider {
 	private DataProvider() {
 		properties = new Properties();
 		userID = properties.getProperty(USER_ID);
+		rootReposPath = properties.getProperty(ROOT_REPOS_PATH);
 
 		try {
 			retrieveTasks();
@@ -53,7 +56,11 @@ public class DataProvider {
 	private Task builtTaskFromString(String taskName) throws IOException {
 		String[] repoData = properties.getProperty(taskName).trim().split(";");
 
-		Task task = new Task(userID, taskName, repoData[0], repoData[1], repoData[2]);
+		String commitOrigin = repoData[0];
+		String repoPath = rootReposPath + File.separator + repoData[1] + File.separator + ".git";
+		String commitID = repoData[2];
+
+		Task task = new Task(userID, taskName, commitOrigin, repoPath, commitID);
 		return task;
 	}
 
