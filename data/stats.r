688dd50af9a146d8afd4973f91a09c7c4bfaf53b
cdmihai <codobanmihai@gmail.com>
2014-03-03 21:35:39 -0800

    loaded survey data. rename

--- data/stats.r
+++ data/stats.r
@@ -2,15 +2,27 @@ multiplyData <- function(vector, multiplicationFactor){
 	return(rep(vector, multiplicationFactor))
 }
 
-data <- read.csv("analysis/results.csv", header=TRUE)
+originalData <- read.csv("analysis/results.csv", header=TRUE)
+survey <- read.csv("survey.csv", header=TRUE)
 
-participants <- unique(data$participant)
+
+
+
+
+
+
+
+
+
+
+
+participants <- unique(originalData$participant)
 
 svn <- vector()
 git <- vector()
 
 for (participant in participants){
-	pData <- data[data$participant == participant, ]
+	pData <- originalData[originalData$participant == participant, ]
 
 	svnData <- pData[pData$commitOrigin == "SVN",]
 	gitData <- pData[pData$commitOrigin == "Git",]
